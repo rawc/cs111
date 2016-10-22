2c98829eeb40053338b5b36b87c529b0ac727fa5
Louie Chatta <louiechatta36@gmail.com>
2016-10-21 17:36:43 -0700

    Add files via upload

--- myshell.c
+++ myshell.c
@@ -1,63 +1,19 @@
 //myshell.c
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <errno.h>
 
-void execute(char **, int, char **);
-int parse(char **, char **, int *);
+extern char **getline();
 
-#define DEFAULT_MODE         0
-#define BACKGROUND_MODE      1
-#define OUTPUT_REDIRECT_MODE 2
-#define INPUT_REDIRECT_MODE  3
-#define PIPELINE_MODE        4
+int main() {
+	int i;
+ 	char **args;
 
-extern char **get_line();
+ 	while(1) {
+ 		args = getline();
+ 		for(i = 0; args[i] != NULL; i++) {
+ 			printf("Argument %d: %s\n", i, args[i]);
+ 		}
+ }
 
-int main(int argc, char *argv[]) {
-	int mode = DEFAULT_MODE;
-	int commandArgc;
-	char *supp = NULL;
-	char **args; 
-	while(1) {
-		printf("myShell>"); //display prompt
-		args = getline();
-		if(strcmp(args[0], "exit") == 0) exit(0);
-		commandArgc = parse(args, &supp, &mode);
-		if(strcmp(args[0], "cd") == 0) chdir(args[1]);
-		else execute(args, mode, &supp);
-		printf("command arg count: %d\n", commandArgc);
-	}
-	return 0;
-}
-
-int parse(char **input, char **suppPtr, int *modePtr) {
-	int i, commandArgc = 0;
-	for(i = 0; input[i] != NULL; i++) {
-		commandArgc++;
-		int pid = fork();
-		printf("pid %d",pid);
-
-		if (strcmp(input[i], "&") == 0) {
-			*modePtr = BACKGROUND_MODE;
-			printf("running in Background Mode\n");
-		} else if (strcmp(input[i], ">") == 0) {
-			*modePtr = OUTPUT_REDIRECT_MODE;
-			printf("running in Output Redirect Mode\n");
-		} else if (strcmp(input[i], "<") == 0) {
-			*modePtr = INPUT_REDIRECT_MODE;
-			printf("running in Input Redirect Mode\n");
-		} else if (strcmp(input[i], "|") == 0) {
-			*modePtr = PIPELINE_MODE;
-			printf("running in Pipeline Mode\n");
-		}
-		printf("Argument %d: %s\n", i, input[i]);
-	}
-	return commandArgc;
-}
-
-void execute(char **command, int mode, char **suppPtr) {
-   printf("executing\n");
-}
