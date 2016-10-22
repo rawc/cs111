779a9aa2bb3d7a3b5862852735a397069ba0dcdf
Louie Chatta <louiechatta36@gmail.com>
2016-10-21 17:50:23 -0700

    Add files via upload

--- myshell.c
+++ myshell.c
@@ -5,15 +5,28 @@
 #include <errno.h>
 
 extern char **getline();
+#define EXIT = "exit"
 
 int main() {
 	int i;
  	char **args;
 
+ 	// main loop
  	while(1) {
- 		printf("$");
+ 		printf("$ ");
  		args = getline();
+
  		for(i = 0; args[i] != NULL; i++) {
+ 			char* cmd;
+ 			if (i == 0) {
+ 				// command is here
+ 				cmd = args[i];
+
+ 				if (strcmp(cmd,EXIT) == 0) {
+ 					sys.exit(0);
+ 				}
+
+ 			}
  			printf("Argument %d: %s\n", i, args[i]);
  		}
  	}
